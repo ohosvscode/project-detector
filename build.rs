5e3c887edf8272c4ea8ff6c597b88f192c87f2d1
groupguanfang <1203970284@qq.com>
2025-10-13 20:38:52 +0800

    fix: try to fix unit test

--- build.rs
+++ build.rs
@@ -1,5 +1,16 @@
 extern crate napi_build;
 
+use std::env;
+
 fn main() {
   napi_build::setup();
+
+  // 在 Linux GNU 的 arm64/armv7 目标上链接 libbsd，提供 le16toh 等符号
+  let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
+  let target_env = env::var("CARGO_CFG_TARGET_ENV").unwrap_or_default();
+  let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap_or_default();
+
+  if target_os == "linux" && target_env == "gnu" && (target_arch == "aarch64" || target_arch == "arm") {
+    println!("cargo:rustc-link-lib=bsd");
+  }
 }
