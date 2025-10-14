f788207d056ec8ced81a5ab9102cfa95a869f6b2
groupguanfang <1203970284@qq.com>
2025-10-14 10:52:51 +0800

    chore: fix ci

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -4,4 +4,5 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   entry: './src/node/index.ts',
   external: [/\.node$/, path.resolve('index.js')],
+  platform: 'node', // 明确指定平台为 node
 })
