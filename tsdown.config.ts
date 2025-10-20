26be4175331c1511f0c9dd11c39431df554cc95e
groupguanfang <1203970284@qq.com>
2025-10-20 18:00:42 +0800

    fix: entry point

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,27 +3,36 @@ import { dts } from 'rolldown-plugin-dts'
 import { defineConfig } from 'tsdown/config'
 import { createDtsResolvePlugin } from './scripts/resolve-plugin'
 
-export default defineConfig({
-  entry: './src/node/index.ts',
-  external: [/\.node$/, path.resolve('index.js')],
-  dts: false,
-  plugins: [
-    dts()
-      .filter(plugin => plugin.name !== 'rolldown-plugin-dts:resolver')
-      .concat(
-        createDtsResolvePlugin({
-          tsconfig: path.resolve('tsconfig.json'),
-          resolve: false,
-        }),
-      ),
-  ],
-  outputOptions: {
-    advancedChunks: {
-      groups: [
+export default defineConfig([
+  {
+    entry: './src/node/index.ts',
+    external: [/\.node$/, path.resolve('index.js')],
+    dts: false,
+    format: ['esm', 'cjs'],
+  },
+  {
+    entry: './src/node/index.ts',
+    external: [/\.node$/, path.resolve('index.js')],
+    dts: false,
+    format: 'esm',
+    plugins: [
+      dts()
+        .filter(plugin => plugin.name !== 'rolldown-plugin-dts:resolver')
+        .concat(
+          createDtsResolvePlugin({
+            tsconfig: path.resolve('tsconfig.json'),
+            resolve: false,
+          }),
+        ),
+    ],
+    outputOptions: {
+      advancedChunks: {
+        groups: [
         // handle .d.ts files
-        { test: /module-build-profile.*\.d\.[cm]?ts$/, name: 'module-build-profile.d' },
-        { test: /project-build-profile.*\.d\.[cm]?ts$/, name: 'project-build-profile.d' },
-      ],
+          { test: /module-build-profile.*\.d\.[cm]?ts$/, name: 'module-build-profile.d' },
+          { test: /project-build-profile.*\.d\.[cm]?ts$/, name: 'project-build-profile.d' },
+        ],
+      },
     },
   },
-})
+])
