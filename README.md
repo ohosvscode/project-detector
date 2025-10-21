35bf6488d6c77dc8eca21a886345883f86fb2a06
groupguanfang <1203970284@qq.com>
2025-10-21 21:03:03 +0800

    chore: update deps

--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 指定一个基础的工作目录，扫描这个目录下的所有鸿蒙工程。
 
 提供`napi`绑定，用于在 Node.js 中使用。
+
+## 关于 `bundled` 包
+
+`bundled` 包将node.js层的封装包含 `node_modules` 中的依赖一起打包，可以直接在 `Node.js` / `Bun` 中直接导入使用（可以无需依赖安装），避免了依赖安装不全的问题。导入直接改为这样即可：
+
+```ts
+import { ProjectDetector } from '@arkts/project-detector/bundled'
+```
--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,32 +1,64 @@
 import path from 'node:path'
 import { dts } from 'rolldown-plugin-dts'
 import { defineConfig } from 'tsdown/config'
+import { dependencies } from './package.json'
 import { IndexExternalResolvePlugin } from './scripts/index-external-plugin'
 import { createDtsResolvePlugin } from './scripts/resolve-plugin'
 
-export default defineConfig({
-  entry: './src/node/index.ts',
-  external: [/\.node$/, path.resolve('index.js')],
-  dts: false,
-  format: 'esm',
-  plugins: [
-    IndexExternalResolvePlugin(path.resolve('index'), '../index.js'),
-    dts()
-      .filter(plugin => plugin.name !== 'rolldown-plugin-dts:resolver')
-      .concat(
-        createDtsResolvePlugin({
-          tsconfig: path.resolve('tsconfig.json'),
-          resolve: false,
-        }),
-      ),
-  ],
-  outputOptions: {
-    advancedChunks: {
-      groups: [
-        // handle .d.ts files
-        { test: /module-build-profile.*\.d\.[cm]?ts$/, name: 'module-build-profile.d' },
-        { test: /project-build-profile.*\.d\.[cm]?ts$/, name: 'project-build-profile.d' },
-      ],
+export default defineConfig([
+  {
+    entry: './src/node/index.ts',
+    external: [/\.node$/, path.resolve('index.js')],
+    dts: false,
+    format: 'esm',
+    plugins: [
+      IndexExternalResolvePlugin(path.resolve('index'), '../index.js'),
+      dts()
+        .filter(plugin => plugin.name !== 'rolldown-plugin-dts:resolver')
+        .concat(
+          createDtsResolvePlugin({
+            tsconfig: path.resolve('tsconfig.json'),
+            resolve: false,
+          }),
+        ),
+    ],
+    outputOptions: {
+      advancedChunks: {
+        groups: [
+          // handle .d.ts files
+          { test: /module-build-profile.*\.d\.[cm]?ts$/, name: 'module-build-profile.d' },
+          { test: /project-build-profile.*\.d\.[cm]?ts$/, name: 'project-build-profile.d' },
+        ],
+      },
     },
   },
-})
+  {
+    entry: {
+      bundled: './src/node/index.ts',
+    },
+    external: [/\.node$/, path.resolve('index.js')],
+    noExternal: Object.keys(dependencies),
+    dts: false,
+    format: 'esm',
+    plugins: [
+      IndexExternalResolvePlugin(path.resolve('index'), '../index.js'),
+      dts()
+        .filter(plugin => plugin.name !== 'rolldown-plugin-dts:resolver')
+        .concat(
+          createDtsResolvePlugin({
+            tsconfig: path.resolve('tsconfig.json'),
+            resolve: false,
+          }),
+        ),
+    ],
+    outputOptions: {
+      advancedChunks: {
+        groups: [
+          // handle .d.ts files
+          { test: /module-build-profile.*\.d\.[cm]?ts$/, name: 'module-build-profile.d' },
+          { test: /project-build-profile.*\.d\.[cm]?ts$/, name: 'project-build-profile.d' },
+        ],
+      },
+    },
+  },
+])
